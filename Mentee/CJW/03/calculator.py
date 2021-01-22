cf301556301a1f0c5565605d59e20c984783720a
jiwoo4424 <wldn637@khu.ac.kr>
2021-01-22 16:49:48 +0900

    [S_Ref] Day04 Homework

--- Mentee/CJW/03/calculator.py
+++ Mentee/CJW/03/calculator.py
@@ -1,65 +1,58 @@
-global_number = 0
+class calculator :
 
-
-class calculator:
+    def __init__(self):
+        self.result = 0
 
     def add(self, input_number):
-        global global_number
-        global_number = global_number + input_number
-        return global_number
+        self.result = self.result + input_number
+        return self.result
 
     def sub(self, input_number):
-        global global_number
-        global_number = global_number - input_number
-        return global_number
+        self.result = self.result - input_number
+        return self.result
 
     def mul(self, input_number):
-        global global_number
-        global_number = global_number * input_number
-        return global_number
+        self.result = self.result * input_number
+        return self.result
 
     def div(self, input_number):
-        global global_number
-        global_number = global_number / input_number
-        return global_number
+        if input_number == 0 :
+            print('Error')
+        else :
+            self.result = self.result/input_number
 
-    def to_zero(self):
-        global global_number
-        global_number = 0
-        return global_number
-
+    def to_zero(self, input_number):
+        self.result = 0
+        return self.result
 
 print('초기의 값은 0입니다')
 
-object = calculator()
+cal = calculator()
 
 while True:
-    sign = input('기호를 입력해주세요(+, -, /, *, CE ) : ')
-
+    sign = input('기호를 입력해주세요( +, -, /, * ) : ')
     input_number = int(input('숫자를 입력해주세요 : '))
     end = input('계산 결과를 얻고 싶다면 등호를 입력해주세요 :')
 
     if end == '=':
         if sign == '+':
-            object.add(input_number)
-            print(global_number)
+            print(cal.add(input_number))
 
         elif sign == '-':
-            object.sub(input_number)
-            print(global_number)
+            print(cal.sub(input_number))
 
         elif sign == '*':
-            object.mul(input_number)
-            print(global_number)
+            print(cal.mul(input_number))
 
         elif sign == '/':
-            object.div(input_number)
-            print(global_number)
+            print(cal.div(input_number))
+
+    else :
+        print('Error')
 
     reset = input('계산기를 종료하고 싶으면 END, 계속하시려면 아무키, 처음부터 다시 계산하고 싶으면 CE를 입력해주세요 : ')
     if reset == 'END':
         break
 
     elif reset == 'CE':
-        object.to_zero()
-        print(global_number)
+        print(cal.to_zero(input_number))
